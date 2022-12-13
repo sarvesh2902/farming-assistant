dbe14bacab73ed8fee4800020d1421e207bdf032
Sarvesh Patil <sarvesh2902@gmail.com>
2022-12-13 09:54:35 +0530

    solved frontend build errors

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,18 +13,20 @@ const Navbar = () => {
                         <div className=" h-25 flex items-center justify-between">
                             <div className="flex items-center">
                                 <div className="flex-shrink-0 ">
-                                    <a href="/" className="flex items-center">
-                                        <Image
-                                            src="https://res.cloudinary.com/atharva7/image/upload/v1664640503/samples/plantifylogo_ez5l1p.png"
-                                            className="mr-3 h-12 animate-pulse sm:h-24"
-                                            height={95}
-                                            width={95}
-                                            alt="Demeter Logo"
-                                        />
-                                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                                            Demeter
-                                        </span>
-                                    </a>
+                                    <Link href="/">
+                                        <a className="flex items-center">
+                                            <Image
+                                                src="https://res.cloudinary.com/atharva7/image/upload/v1664640503/samples/plantifylogo_ez5l1p.png"
+                                                className="mr-3 h-12 animate-pulse sm:h-24"
+                                                height={95}
+                                                width={95}
+                                                alt="Demeter Logo"
+                                            />
+                                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                                                Demeter
+                                            </span>
+                                        </a>
+                                    </Link>
                                 </div>
                                 <div
                                     className="hidden w-full md:block md:w-auto"
@@ -31,13 +34,14 @@ const Navbar = () => {
                                 >
                                     <ul className="absolute right-0 top-7 flex flex-col rounded-lg border border-gray-100 bg-gray-50 mr-7 mb-3 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-cyan-900">
                                         <li>
-                                            <a
-                                                href="/"
-                                                className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                                                aria-current="page"
-                                            >
-                                                Home
-                                            </a>
+                                            <Link href="/">
+                                                <a
+                                                    className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                                                    aria-current="page"
+                                                >
+                                                    Home
+                                                </a>
+                                            </Link>
                                         </li>
                                         {/* <li>
                                             <a
@@ -48,28 +52,25 @@ const Navbar = () => {
                                             </a>
                                         </li> */}
                                         <li>
-                                            <a
-                                                href="/Disease"
-                                                className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                                            >
-                                                Disease
-                                            </a>
+                                            <Link href="/Disease">
+                                                <a className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
+                                                    Disease
+                                                </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a
-                                                href="/Crop"
-                                                className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                                            >
-                                                Crop
-                                            </a>
+                                            <Link href="/Crop">
+                                                <a className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
+                                                    Crop
+                                                </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a
-                                                href="/Fertilizer"
-                                                className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                                            >
-                                                Fertilizer
-                                            </a>
+                                            <Link href="/Fertilizer">
+                                                <a className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
+                                                    Fertilizer
+                                                </a>
+                                            </Link>
                                         </li>
                                         {/* <li>
                                             <a
@@ -80,12 +81,11 @@ const Navbar = () => {
                                             </a>
                                         </li> */}
                                         <li>
-                                            <a
-                                                href="/Weather"
-                                                className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                                            >
-                                                Weather
-                                            </a>
+                                            <Link href="/Weather">
+                                                <a className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
+                                                    Weather
+                                                </a>
+                                            </Link>
                                         </li>
                                     </ul>
                                 </div>
@@ -162,12 +162,11 @@ const Navbar = () => {
                                     ref={ref}
                                     className="space-y-1 px-2 pt-2 pb-3 sm:px-3"
                                 >
-                                    <a
-                                        href="/"
-                                        className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700"
-                                    >
-                                        Home
-                                    </a>
+                                    <Link href="/">
+                                        <a className="block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-700">
+                                            Home
+                                        </a>
+                                    </Link>
 
                                     {/* <a
                                         href="#"
@@ -176,40 +175,35 @@ const Navbar = () => {
                                         About Us
                                     </a> */}
 
-                                    <a
-                                        href="/Disease"
-                                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                    >
-                                        Disease
-                                    </a>
+                                    <Link href="/Disease">
+                                        <a className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                                            Disease
+                                        </a>
+                                    </Link>
 
-                                    <a
-                                        href="/Crop"
-                                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                    >
-                                        Crop
-                                    </a>
+                                    <Link href="/Crop">
+                                        <a className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                                            Crop
+                                        </a>
+                                    </Link>
 
-                                    <a
-                                        href="/Fertilizer"
-                                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                    >
-                                        Fertilizer
-                                    </a>
+                                    <Link href="/Fertilizer">
+                                        <a className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">
+                                            Fertilizer
+                                        </a>
+                                    </Link>
                                     {/* <a
                                         href="/Profit"
                                         className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                                     >
                                         Profit
                                     </a> */}
-                                     
-                                            <a
-                                                href="/Weather"
-                                                className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
-                                            >
-                                                Weather
-                                            </a>
-                                      
+
+                                    <Link href="/Weather">
+                                        <a className="duration-0 block rounded py-2 pr-4 pl-3 text-xl text-gray-100 transition hover:scale-125 hover:bg-gray-100 hover:duration-150 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
+                                            Weather
+                                        </a>
+                                    </Link>
                                 </div>
                             </div>
                         )}
